8a5f25553a6d1d59da8f362923b8f1b8dfb13697
michiel.govaerts4@gmail.com <michiel.govaerts4@gmail.com>
2019-03-27 12:41:10 +0100

    speler constructor

--- lab4/src/domain/Speler.java
+++ lab4/src/domain/Speler.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Speler {
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private final int volgnr;
+    private Score score;
+
+    public Speler(){
+        this.score = new Score(0);
+        this.volgnr = count.incrementAndGet();
+    }
+
+    public int getVolgnr(){
+        return this.volgnr;
+    }
+
+    public Score getScore(){
+        return this.score;
+    }
 }
